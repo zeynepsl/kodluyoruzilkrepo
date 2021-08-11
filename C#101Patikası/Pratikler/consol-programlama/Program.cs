71a321725892686d3728e11496eabdfe5ed4344b
zeynepsl <zeyneprsalman@gmail.com>
2021-08-11 14:27:12 +0300

    C# 101 patikası

---
+++ C#101Patikası/Pratikler/consol-programlama/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+//System name space i altında Console sınıfı var
+namespace consol_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {        
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("İsminizi girin: ");
+            String name = Console.ReadLine();
+            
+            Console.WriteLine("Soy isminizi girin: ");
+            String surName = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surName);
+        }
+    }
+}
