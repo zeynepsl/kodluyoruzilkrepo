f4df186206c69f1a49cc073d95fe6fb7d84ef9a3
zeynepsl <zeyneprsalman@gmail.com>
2021-08-11 14:12:23 +0300

    C#101Patikası

---
+++ C#101Patikası/demos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace demos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            /**/
+            Console.WriteLine("isminiz: ");
+            String name = Console.ReadLine();
+
+            Console.WriteLine("soy adınız: ");
+            String surname = Console.ReadLine();
+
+            Console.WriteLine("merhaba " + name + " " + surname);
+        }
+    }
+}
